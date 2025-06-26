8f01214741c36bda371013ee3d0da6cd5bd089d9
Kenny Houston <kennyhouston20@gmail.com>
2025-06-26 11:15:58 +0300

    update_sort_by_course

--- sort_by_course.js
+++ sort_by_course.js
@@ -1,15 +1,9 @@
 function initializeSortingCourse() {
     console.log("initializeSortingCourse called.");
 
-    // Убедитесь, что это правило есть в вашем style.css и загружается после Bootstrap
-    // .hidden-card {
-    //     display: none !important;
-    // }
-
     $(".sort-select").on('change', function() {
         console.log("Sort select changed.");
 
-        // Получаем актуальную коллекцию карточек при каждом изменении
         var $cards = $("#cards .my-card"); 
         console.log("Number of cards found:", $cards.length);
 
@@ -17,25 +11,16 @@ function initializeSortingCourse() {
         console.log("Selected target:", selectedTarget);
 
         if (selectedTarget === "all") {
-            // Если выбран "Все курсы", показываем все карточки.
-            // Удаляем класс hidden-card и добавляем d-flex (если он нужен для отображения flex-контейнера)
+
             $cards.removeClass('hidden-card').addClass('d-flex'); 
             console.log("Showing all cards by removing hidden-card and adding d-flex.");
         } else {
-            // Если выбран конкретный курс:
-            // 1. Скрываем все карточки: добавляем hidden-card и удаляем d-flex
-            $cards.addClass('hidden-card').removeClass('d-flex'); 
-            
-            // 2. Находим отфильтрованные карточки
+            $cards.addClass('hidden-card').removeClass('d-flex');           
             var $filteredCards = $cards.filter("." + selectedTarget);
-            
-            // 3. Показываем отфильтрованные: удаляем hidden-card и добавляем d-flex
             $filteredCards.removeClass('hidden-card').addClass('d-flex');
             console.log("Showing filtered cards (class: ." + selectedTarget + "), found:", $filteredCards.length);
         }
     });
 
-    // Опционально: Инициируем сортировку при загрузке страницы, чтобы сразу показать все карточки
-    // Это имитирует выбор "Все курсы"
     $(".sort-select").trigger('change'); 
 }
